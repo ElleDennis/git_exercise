d5ace382abe7eba1c2011a65863a08f2c8388ef4
elle <elledennis@att.net>
2016-03-02 12:27:43 +0000

    Day 4

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+ def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+   def create
+     @user = User.new(params.require(:gram).permit(:name, :email))
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+
+      redirect_to users_path
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  root 'grams#index'
+
+  resources :grams
+  resources :users
+
+  # get 'signup' => 'users#new'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+    # root 'users#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
