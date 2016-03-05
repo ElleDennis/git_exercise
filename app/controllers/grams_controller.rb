6bec344d4696becb1a4da1435ae3068706d13279
elle <elledennis@att.net>
2016-03-05 02:29:06 +0000

    Last morn

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,7 +8,8 @@ class GramsController < ApplicationController
   end
 
   def create
-     @gram = Gram.new(params.require(:gram).permit(:title, :content, :country_code))
+     @gram = Gram.new(params.require(:gram).permit(:title, :content, :country_code, :image, :image_cache))
+
     if @gram.save
       flash[:notice] = 'Gram created!'
       redirect_to @gram
