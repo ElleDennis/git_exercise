0c8d52cc08097732478ab313450d310d1ccdd0df
elle <elledennis@att.net>
2016-04-12 09:03:52 +0000

    Notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
      @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
 
     if @user.save
-      redirect_to @user
+      redirect_to @user, "Sign up was successful. Please sign in."
     else
       render :new
     end
