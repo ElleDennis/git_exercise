1f46898bbcb29d8cf4f8d957aba8e43a81e8e81b
elle <elledennis@att.net>
2016-04-27 07:12:56 +0000

    Add subscribed to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :grams
+  has_one :subscription, :dependent => :destroy
   validates_presence_of   :first_name, :last_name, :email
   validates_format_of     :email, with: /@/
   validates_uniqueness_of :email, case_sensitive: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425024352) do
+ActiveRecord::Schema.define(version: 20160427070029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,9 @@ ActiveRecord::Schema.define(version: 20160425024352) do
     t.string   "password_digest"
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "subscribed",       :default => false
   end
 
-  add_index "users", ["email"], first_name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
